84ad984eaf986f034b212b572fbc5b89892b0e1a
AIon-Gbbe <developer.andrasesc.ionut@gmail.com>
2017-07-08 02:53:27 +0300

    fixed remote js

--- example/main.js
+++ example/main.js
@@ -4,6 +4,6 @@ const Elm = require('./Example.elm');
 let app = Elm.Example.embed(document.getElementById('main'));
 
 // require('elm-pdf').elmPdfInitialize(app);
-require('./localPdf.js').elmPdfInitialize(app);
+require('elm-pdf').elmPdfInitialize(app);
 
 // console.log("elmPdfff", elmPdf);
